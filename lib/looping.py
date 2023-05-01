c8d8167c913dd18bb23a18cebbd35098a9391186
Clifton Gockul <clifga@gmail.com>
2023-05-01 13:05:03 -0400

    completed:

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i > 0):
+        print(i)
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_number = [intnum * intnum for intnum in int_list]
+    return new_number
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+            continue
+        elif i % 5 == 0:
+            print("Buzz")
+            continue
+        else:
+            print(i)
+            continue
